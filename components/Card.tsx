92d621839924f99525a72b84dc1985a30f2d506f
supanut <supanut.tan@ondemand.in.th>
2025-02-01 10:28:07 +0700

    feat(files): integtate fetch files api

---
+++ components/Card.tsx
@@ -0,0 +1,8 @@
+import { Models } from 'node-appwrite';
+import React from 'react';
+
+const Card = ({ file }: { file: Models.Document }) => {
+  return <div>{file.name}</div>;
+};
+
+export default Card;
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -3,9 +3,15 @@
 import { createAdminClient } from '@/lib/appwrite';
 import { InputFile } from 'node-appwrite/file';
 import { appwirteConfig } from '../appwrite/config';
-import { ID } from 'node-appwrite';
+import { ID, Models, Query } from 'node-appwrite';
 import { constructFileUrl, getFileType, parseStringify } from '../utils';
 import { revalidatePath } from 'next/cache';
+import { getCurrentUser } from './user.actions';
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
 
 export const uploadFile = async ({
   file,
@@ -55,7 +61,37 @@ export const uploadFile = async ({
   }
 };
 
-const handleError = (error: unknown, message: string) => {
-  console.log(error, message);
-  throw error;
+const createQueries = (currentUser: Models.Document) => {
+  const queries = [
+    Query.or([
+      Query.equal('owner', [currentUser.$id]),
+      Query.contains('users', [currentUser.email]),
+    ]),
+  ];
+
+  // TODO: Search, sort, limits, ...
+
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error('User not found');
+
+    const queries = createQueries(currentUser);
+
+    const files = await databases.listDocuments(
+      appwirteConfig.databaseId,
+      appwirteConfig.filesCollectionId,
+      queries
+    );
+
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, 'Failed to get files');
+  }
 };
