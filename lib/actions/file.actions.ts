c706b40be7a7e7c1c0cf254fe147e4ccc89a2c5e
supanut <supanut.tan@ondemand.in.th>
2025-02-08 16:47:26 +0700

    feat(filter): filter by file type

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -61,7 +61,7 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[]) => {
   const queries = [
     Query.or([
       Query.equal('owner', [currentUser.$id]),
@@ -69,12 +69,14 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
+  if (types.length > 0) queries.push(Query.equal('type', types));
+
   // TODO: Search, sort, limits, ...
 
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [] }: GetFilesProps) => {
   const { databases } = await createAdminClient();
 
   try {
@@ -82,7 +84,7 @@ export const getFiles = async () => {
 
     if (!currentUser) throw new Error('User not found');
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types);
 
     const files = await databases.listDocuments(
       appwirteConfig.databaseId,
